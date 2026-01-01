cff3fca910b21c4d33690328d8bed2f5fe9078a1
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2026-01-01 17:37:27 +0100

    feat: updated wearedevs spider to yield Jobscraper items

--- backend/src/scraping/scraper/items.py
+++ backend/src/scraping/scraper/items.py
@@ -4,4 +4,7 @@ from scrapy import Item, Field
 class JobscraperItem(Item):
     title = Field()
     description = Field()
+    skills = Field()
+    location = Field()
+    seniority_levels = Field()
     url = Field()
--- backend/src/scraping/scraper/spiders/wearedevs_spider.py
+++ backend/src/scraping/scraper/spiders/wearedevs_spider.py
@@ -1,5 +1,4 @@
-from pathlib import Path
-
+from ..items import JobscraperItem
 import scrapy
 
 
@@ -9,27 +8,31 @@ class WeAreDevelopersSpider(scrapy.Spider):
 
     def parse(self, response):
         for job in response.css(".wad4-job-card"):
-            title = job.css("h3.wad4-job-card__title::text").get().strip()
+            title = job.css("h3.wad4-job-card__title::text").get(default="").strip()
+            seniority_levels = job.css("div.wad4-common-chip--seniority span.wad4-common-chip__label::text").getall()
+            location = job.css("span.wad4-job-card__info--light::text").get(default="").strip()
             job_link = job.css("a.wad4-job-card__link::attr(href)").get()
             if job_link:
-                yield response.follow(job_link, callback=self.parse_job, meta={'title': title})
+                yield response.follow(job_link, callback=self.parse_job, meta={'title': title, 'location': location, 'seniority_levels': seniority_levels})
 
     def parse_job(self, response):
-        title = response.meta['title']
-        sections = response.css("h2.wad4-job-details-section__title")
-        contents = response.css("div.wad4-job-details__html-content")
+        def get_section_text(section_name):
+            sections = response.css("h2.wad4-job-details-section__title")
+            for h2 in sections:
+                section_title = h2.css("::text").get(default="").strip().lower()
+                if section_name.lower() in section_title:
+                    div = h2.xpath("following-sibling::div[1]")
+                    return " ".join(div.css("*::text").getall()).strip()
+            return ""
 
-        job_data = {
-            'title': title,
-            'link': response.url,
-            'description': None
-        }
+        job_item = JobscraperItem()
+        job_item['url'] = response.url
+        job_item['title'] = response.meta['title']
+        job_item['location'] = response.meta['location']
+        job_item['seniority_levels'] = response.meta['seniority_levels']
+        job_item['description'] = get_section_text("job description")
 
-        for h2, div in zip(sections, contents):
-            section_title = h2.css("::text").get().strip().lower()
-            section_text = " ".join(div.css("*::text").getall()).strip()
+        skills = response.css("div.wad4-job-details__skills span.wad4-common-chip__label::text").getall()
+        job_item['skills'] = [ skill.strip() for skill in skills if skill.strip() ]
 
-            if "job description" in section_title:
-                job_data['description'] = section_text
-
-        yield job_data
+        yield job_item
