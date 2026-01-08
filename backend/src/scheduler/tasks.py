4e9ca2dd1fcd02d28f078f78cdb8e5196b0c6b13
Rostyslav Nikolenko <rostislavnikolenkowork@gmail.com>
2026-01-08 18:16:06 +0100

    feat: added docker compose file and implemented scheduled scraping

--- backend/src/scheduler/tasks.py
+++ backend/src/scheduler/tasks.py
@@ -1,6 +1,7 @@
+import subprocess
 from src.scheduler.app import celery
 
 
 @celery.task(time_limit=1800) 
 def scrape_task():
-    pass 
+    subprocess.run(["scrapy", "crawl", "wearedevs"], check=True)
