8e14ba858f79c020a0824d3b025afe58eaf238d7
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2026-01-09 21:32:11 +0100

    chore: small frontend tweak

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -21,4 +21,4 @@ app.add_middleware(
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("__main__:app", host="0.0.0.0", port=8000, reload=settings.DEBUG)
+    uvicorn.run("src.api.main:app", host="0.0.0.0", port=8000, reload=settings.DEBUG)
