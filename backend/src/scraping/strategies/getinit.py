acec656df7b07e2ff12f1d9bd14d4ca10dda3285
Rostyslav Nikolenko <rostislavnikolenkowork@gmail.com>
2026-01-17 12:16:21 +0100

    feat: fully implemented getinit spider

--- backend/src/scraping/strategies/getinit.py
+++ backend/src/scraping/strategies/getinit.py
@@ -2,16 +2,17 @@ from src.scraping.strategies.base import JobExtractionStrategy
 
 
 class GetInItStrategy(JobExtractionStrategy):
-    def __init__(self):
-        self.company = "placeholder"
-        
+    def extract_country(self, response):
+        return "germany"
+    
+    def extract_company(self, response):
+        return response.css("p[class^='JobHeaderRegular_companyTitle']::text").get(default="")
+    
     def extract_location(self, response) -> str:
         self.location = response.css("div.ms-1-5.d-block p::text").get(default="")
         return self.location
-    
-    def extract_country(self, response):
-        return "germany"
 
     def extract_description(self, response) -> str:
-        self.description = "placeholder"
+        description_info = response.css("div.row div.col-12.offset-lg-1.col-lg-7 ::text").getall()
+        self.description = " ".join(description_info)
         return self.description
