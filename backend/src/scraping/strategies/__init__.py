6a0d3204dbfce012af0f952a2dc7e6e39281f0ef
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2026-01-25 12:46:01 +0100

    chore: changed spider file names to improve naming consistency

--- backend/src/scraping/strategies/__init__.py
+++ backend/src/scraping/strategies/__init__.py
@@ -3,6 +3,6 @@ from src.scraping.strategies.getinit import GetInItStrategy
 from src.scraping.strategies.relocateme import RelocateMeStrategy
 from src.scraping.strategies.sap import SapStrategy
 from src.scraping.strategies.siemens import SiemensStrategy
-from src.scraping.strategies.wearedevs import WeAreDevsStrategy
+from src.scraping.strategies.wearedevelopers import WeAreDevsStrategy
 from src.scraping.strategies.zalando import ZalandoStrategy
 from src.scraping.strategies.dice import DiceStrategy
