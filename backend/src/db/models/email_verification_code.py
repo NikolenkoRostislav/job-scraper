9b7a686befa52703c48222027258478a214588a8
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2026-01-27 21:02:54 +0100

    feat: added email verification codes table

---
+++ backend/src/db/models/email_verification_code.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from sqlalchemy import DateTime, text
+from sqlalchemy.orm import Mapped, mapped_column
+from src.db.database import Base
+
+
+class EmailVerificationCode(Base):
+    __tablename__ = "email_verification_codes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True)
+    code: Mapped[int] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=text("CURRENT_TIMESTAMP"))
